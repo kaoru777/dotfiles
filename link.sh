61221ca505e13e5173d36a44fadaf6bccf4e0793
kaoru <kaoru@steinboeck.dev>
2023-01-02 21:48:22 +0100

    modified link.sh

--- link.sh
+++ link.sh
@@ -2,4 +2,5 @@
 ln -s ~/dotfiles/.zshenv  ~/.zshenv
 ln -s ~/dotfiles/.zshrc  ~/.zshrc
 ln -s ~/dotfiles/.psqlrc  ~/.psqlrc
+ln -s ~/dotfiles/.tmux.conf  ~/.tmux.conf
 ln -s ~/dotfiles/.gitignore_global  ~/.gitignore_global
